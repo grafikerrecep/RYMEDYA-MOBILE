8bc24b0fc1731af2b5fbe6566ecbd81dfbf5b991
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2022-08-16 21:53:14 +0300

    home default image added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,39 @@
-import React from "react";
-import { View, Text } from 'react-native';
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 function Home() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.headerImageView}>
+        <Image
+          style={styles.headerImage}
+          source={{
+            uri: 'https://nebosan.com.tr/wp-content/uploads/2018/06/no-image.jpg',
+          }}
+        />
+      </View>
+      <View style={styles.homePageButtonContainer}>
+        <Text>Home Screen</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  headerImageView: {
+    marginTop: 50,
+  },
+  headerImage: {
+    width: 200,
+    height: 120,
+  },
+  homePageButtonContainer: {
+    marginTop: 25,
+  },
+});
 
 export default Home;
